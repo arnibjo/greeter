b7f7add99be2f90215511aff49cc685f14bec56e
arnibjo <abo25@hi.is>
2024-02-06 12:48:04 +0000

    Fixes to compile

--- compile.cmd
+++ compile.cmd
@@ -1,4 +1,4 @@
-javac -d bin src/is/hi/abo25/hbv202g/ass3/backend/Language.java
-javac -d bin src/is/hi/abo25/hbv202g/ass3/frontend/UserInterface.java
-javac -d bin src/is/hi/abo25/hbv202g/ass3/backend/Greeter.java
+javac -d bin -cp bin src/is/hi/abo25/hbv202g/ass3/backend/Language.java
+javac -d bin -cp bin src/is/hi/abo25/hbv202g/ass3/frontend/UserInterface.java
+javac -d bin -cp bin src/is/hi/abo25/hbv202g/ass3/backend/Greeter.java
 javac -d bin -cp bin src/Main.java
